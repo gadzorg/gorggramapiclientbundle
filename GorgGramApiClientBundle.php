db66cf5db6ac1c32ee2c230a72439deb35dc5810
Mathieu GOULIN <mathieu.goulin@gadz.org>
2012-10-10 16:38:35 +0200

    Initial commit of the bundle

---
+++ GorgGramApiClientBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Gorg\Bundle\GramApiClientBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class GorgGramApiClientBundle extends Bundle
+{  
+}
